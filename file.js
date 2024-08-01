b72c8a0435f3f866b06e30463aa1141dd821aeb1
Mobin <mobinzaki368@gmail.com>
2024-08-01 19:39:35 +0330

    fuck js

--- file.js
+++ file.js
@@ -1,3 +1,23 @@
-document.getElementById("myButton").addEventListener("click", function() {
-    window.location.href = "index2.html";
-});
+document.getElementById("myButton").onclick = function() {
+    window.location.href="D:/newsletter-sign-up-with-success-message-main/newsletter-sign-up-with-success-message-main/index.html2";
+}
+
+document.getElementById("dismiss").onclick = function() {
+    window.location.href="/#/newsletter-sign-up-with-success-message-main/index.html";
+}
+
+document.getElementById('emailForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var email = document.getElementById('email').value;
+    localStorage.setItem('userEmail', email);
+    window.location.href="/#/newsletter-sign-up-with-success-message-main/index.html2";
+})
+
+window.onload = function (){
+    var email = localStorage.getItem('userEmail');
+    if (email){
+        document.getElementById('emailForm').textContent = email;
+    }else {
+        document.getElementById('emailForm').textContent = 'NO email found.';
+    }
+}
